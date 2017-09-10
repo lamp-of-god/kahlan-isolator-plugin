09f6426adad12193c5fff6290093b1066f895e8f
SeRRg <sergey_rus@bk.ru>
2017-09-10 11:43:42 +0500

    Init

---
+++ src/Plugin/Isolator.php
@@ -0,0 +1,24 @@
+<?php
+namespace LampOfGod\Plugin;
+
+use Kahlan\Jit\Interceptor;
+use LampOfGod\Jit\Patcher\Isolator as Patcher;
+
+/**
+ * Plugin that allows to isolate functions from given file.
+ */
+class Isolator
+{
+    /**
+     * Performs functions isolation and requires them from given file.
+     *
+     * @param string $file   File path to isolate functions from.
+     */
+    public static function isolate($file)
+    {
+        $interceptor = Interceptor::instance();
+        $interceptor->patchers()->add('isolator', new Patcher());
+        $interceptor->loadFile($file);
+        $interceptor->patchers()->remove('isolator');
+    }
+}
